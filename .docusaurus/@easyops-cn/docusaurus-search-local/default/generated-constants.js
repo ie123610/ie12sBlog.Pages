1b14f206df9b0640a968716ee9b4ed5f19edd92d
ie12 <150226351+ie123610@users.noreply.github.com>
2026-01-27 08:55:02 +0800

    Delete .docusaurus directory

--- .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
+++
@@ -1,9 +0,0 @@
-import lunr from "/home/ie12/文档/GitHub/ie12sBlog.Pages/node_modules/lunr/lunr.js";
-require("/home/ie12/文档/GitHub/ie12sBlog.Pages/node_modules/lunr-languages/lunr.stemmer.support.js")(lunr);
-require("/home/ie12/文档/GitHub/ie12sBlog.Pages/node_modules/@easyops-cn/docusaurus-search-local/dist/client/shared/lunrLanguageZh.js").lunrLanguageZh(lunr);
-require("/home/ie12/文档/GitHub/ie12sBlog.Pages/node_modules/lunr-languages/lunr.multi.js")(lunr);
-export const removeDefaultStopWordFilter = [];
-export const language = ["en","zh"];
-export const searchIndexUrl = "search-index{dir}.json?_=b3bfde39";
-export const searchResultLimits = 8;
-export const fuzzyMatchingDistance = 1;
